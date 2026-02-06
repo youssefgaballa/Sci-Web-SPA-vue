ad4cb5b3361e4e8471eb93fe52e5fce6de2b5e17
Youssef Gaballa <yg01467@uga.edu>
2026-02-06 13:52:37 -0500

    refactor: moved editor toolbar to different component

--- src/common/components/editor/RichTextEditor.vue
+++ src/common/components/editor/RichTextEditor.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import StarterKit from '@tiptap/starter-kit'
 import { useEditor, EditorContent } from '@tiptap/vue-3'
-import { Icon } from '@iconify/vue'
+import EditorToolbar from './EditorToolbar.vue'
 
 const editor = useEditor({
   content: '<p></p>',
@@ -11,38 +11,7 @@ const editor = useEditor({
 
 <template>
   <div class="editor-container" v-if="editor">
-    <div class="editor-buttons-group">
-      <button
-        @click="editor.chain().focus().toggleBold().run()"
-        :class="{ 'is-active': editor!.isActive('bold') }"
-      >
-        <Icon icon="glyphs:b-bold" width="16" height="16" />
-      </button>
-      <button
-        @click="editor.chain().focus().toggleItalic().run()"
-        :class="{ 'is-active': editor!.isActive('italic') }"
-      >
-        <Icon icon="tabler:italic" width="16" height="16" />
-      </button>
-      <button
-        @click="editor.chain().focus().toggleUnderline().run()"
-        :class="{ 'is-active': editor!.isActive('underline') }"
-      >
-        <Icon icon="tabler:underline" width="16" height="16" />
-      </button>
-      <button
-        @click="editor.chain().focus().toggleStrike().run()"
-        :class="{ 'is-active': editor!.isActive('strike') }"
-      >
-        <Icon icon="icon-park-outline:strikethrough" width="16" height="16" />
-      </button>
-      <button
-        @click="editor.chain().focus().toggleCodeBlock().run()"
-        :class="{ 'is-active': editor!.isActive('codeBlock') }"
-      >
-        <Icon icon="fluent:code-block-16-regular" width="16" height="16" />
-      </button>
-    </div>
+    <EditorToolbar :editor="editor" v-if="editor" />
     <editor-content :editor="editor" />
   </div>
 </template>
@@ -53,9 +22,5 @@ const editor = useEditor({
   padding: 10px;
   width: 60%;
   height: 100%;
-}
-
-.editor-buttons-group {
-  margin-bottom: 10px;
 }
 </style>
--- src/pages/CreatePage.vue
+++ src/pages/CreatePage.vue
@@ -3,14 +3,21 @@ import RichTextEditor from '@/common/components/editor/RichTextEditor.vue'
 </script>
 
 <template>
-  <div class="edit-content-container">
-    <RichTextEditor />
-  </div>
+  <form class="create-page-form">
+    <div class="edit-content-container">
+      <RichTextEditor />
+    </div>
+  </form>
 </template>
 
 <style scoped>
 .edit-content-container {
   width: 60%;
   padding: 20px;
+}
+
+.create-page-form {
+  width: 100%;
+  height: 100%;
 }
 </style>
